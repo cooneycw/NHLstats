96edf03a8de6132d9696ccc8556306dbca27243a
cooneycw <cooneycw@icloud.com>
2024-02-24 09:30:53 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from src_code.data_collect.collect_01 import get_game_data
+
+
+def main():
+    get_game_data()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
